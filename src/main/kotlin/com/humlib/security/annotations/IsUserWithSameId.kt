9268e24cd0c5647fb519e8ad3985b4a5efd6f0f6
Morathii <morathii@icloud.com>
2022-03-13 16:44:49 +0100

    :wrench: Rename the users role to 'human' (#5)

--- src/main/kotlin/com/humlib/security/annotations/IsUserWithSameId.kt
+++ src/main/kotlin/com/humlib/security/annotations/IsUserWithSameId.kt
@@ -3,5 +3,5 @@ package com.humlib.security.annotations
 import org.springframework.security.access.prepost.PreAuthorize
 
 @Retention(AnnotationRetention.RUNTIME)
-@PreAuthorize("#id.toString() == authentication.name && hasAuthority('SCOPE_users')")
+@PreAuthorize("#id.toString() == authentication.name && hasAuthority('SCOPE_human')")
 annotation class IsUserWithSameId
