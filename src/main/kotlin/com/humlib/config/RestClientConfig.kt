7a32552a657756526466bbc2edc85d9ac055924b
Morathii <morathii@icloud.com>
2022-03-22 23:00:16 +0100

    :hammer: Clean up unneeded test and unused import

--- src/main/kotlin/com/humlib/config/RestClientConfig.kt
+++ src/main/kotlin/com/humlib/config/RestClientConfig.kt
@@ -10,7 +10,6 @@ import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestHighLevelClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration
 import java.io.BufferedInputStream
 import java.io.FileInputStream
--- src/test/kotlin/com/humlib/DummyTest.kt
+++
@@ -1,9 +0,0 @@
-package com.humlib
-
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
-
-class DummyTest {
-    @Test
-    fun test() = assertTrue(true)
-}
